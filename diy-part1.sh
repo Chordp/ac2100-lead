1fecf8fda44582c8d65f181222d1132fa1b9de60
Chord <1263178881@qq.com>
2021-10-09 15:53:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git opentopd  https://github.com/sirpdboy/luci-theme-opentopd' >>feeds.conf.default
