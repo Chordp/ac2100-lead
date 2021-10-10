913b240130c886a4202332799d5ee46ddcb36804
Chord <1263178881@qq.com>
2021-10-10 14:49:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-git clone https://github.com/Zxilly/UA2F.git package/UA2F
+#git clone https://github.com/Zxilly/UA2F.git package/UA2F
 # Add a feed source
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
