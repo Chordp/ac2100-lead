31718b707924534c550b7c4c6f5888ff3ce7bcb9
Chord <1263178881@qq.com>
2022-12-02 19:28:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git boos https://github.com/Boos4721/OpenWrt-Packages' >>feeds.conf.default
+echo 'src-git boos https://github.com/LOLNiubility/OpenWrt-Packages' >>feeds.conf.default
